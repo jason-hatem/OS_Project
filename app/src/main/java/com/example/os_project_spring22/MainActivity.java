96ac2bfa583122e431917241ddd7dddfeec48a52
jason-hatem <104832384+jason-hatem@users.noreply.github.com>
2022-05-03 18:04:09 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/os_project_spring22/MainActivity.java
+++ app/src/main/java/com/example/os_project_spring22/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
